73ea9067bf2aad1f1c761a39eb894ac59780a000
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-02-13 18:27:40 +0100

    Changed implementation

--- src/balance.rs
+++ src/balance.rs
@@ -9,15 +9,15 @@ pub fn main() {
     let mut buf = Vec::new();
     reader.read_to_end(&mut buf).unwrap();
 
-    let mut stack = VecDeque::with_capacity(buf.len());
+    let mut stack = Vec::with_capacity(buf.len());
     let mut balanced = false;
 
     for c in buf.iter() {
         match c {
-            40 => stack.push_front(40),
-            91 => stack.push_front(91),
+            40 => stack.push(40),
+            91 => stack.push(91),
             41 => {
-                match stack.pop_front() {
+                match stack.pop() {
                     Some(num) => {
                         if num != 40 {
                             break;
@@ -27,7 +27,7 @@ pub fn main() {
                 };
             }
             93 => {
-                match stack.pop_front() {
+                match stack.pop() {
                     Some(num) => {
                         if num != 91 {
                             break;
