8650a6434bfa65f5fb8ea9e90dcea92431bc0c95
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-02-19 13:41:57 +0100

    Seatallocation fixes

--- python/seatallocation.py
+++ python/seatallocation.py
@@ -2,11 +2,11 @@ n, m = map(lambda x: int(x), input().split())
 
 seats = [0] * n
 votes = [float(input()) for _ in range(n)] 
-quotient = votes.copy()
+quotients = votes.copy()
 
-for i in range(m):
-    i_max = quotient.index(max(quotient))
-    quotient[i_max] = (votes[i_max] / (seats[i_max] + 2))
+for _ in range(m):
+    i_max = quotients.index(max(quotients))
+    quotients[i_max] = (votes[i_max] / (seats[i_max] + 2))
     seats[i_max] += 1
     
 for s in seats:
